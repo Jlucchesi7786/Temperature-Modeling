7dd63ead9790b933d947a4ab810a402da3ffa474
Jackson Lucchesi <jacksonlucchesi@RAYMONDs-MacBook-Pro.local>
2020-02-10 12:41:54 -0700

    yer dun

--- Project/src/Sensor.java
+++ Project/src/Sensor.java
@@ -1,5 +1,33 @@
 import java.util.*;
 
 public class Sensor {
-
+	private boolean working;
+	private boolean badData;
+	
+	private double voltage;
+	
+	public Sensor() {
+		working = true;
+		badData = false;
+	}
+	
+	public boolean isWorking() {
+		return working;
+	}
+	
+	public void setWorking(boolean newState) {
+		working = newState;
+	}
+	
+	public boolean hasBadData() {
+		return badData;
+	}
+	
+	public void giveBadData(boolean newState) {
+		badData = newState;
+	}
+	
+	public String toString() {
+		return "";
+	}
 }
