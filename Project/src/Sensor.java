e73d978dc0c45acbd8b67b1af67a652593d54514
Jackson Lucchesi <jacksonlucchesi@RAYMONDs-MacBook-Pro.local>
2020-02-10 18:21:28 -0700

    Test is good

--- Project/src/Sensor.java
+++ Project/src/Sensor.java
@@ -5,28 +5,28 @@ public class Sensor {
 	private boolean badData;
 	
 	private double voltage;
-	
+
 	public Sensor() {
 		working = true;
 		badData = false;
 	}
-	
+
 	public boolean isWorking() {
 		return working;
 	}
-	
+
 	public void setWorking(boolean newState) {
 		working = newState;
 	}
-	
+
 	public boolean hasBadData() {
 		return badData;
 	}
-	
+
 	public void giveBadData(boolean newState) {
 		badData = newState;
 	}
-	
+
 	public String toString() {
 		return "";
 	}
